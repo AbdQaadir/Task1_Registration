7f0de3e7ff379cbd2b9d8064ba4b36faa9790f71
Lateef Quadri Olayinka <Lateef9816@gmail.com>
2020-05-28 23:43:33 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,15 +1,15 @@
 const submitBtn = document.querySelector("#submit");
-const name = document.querySelector("#name").value;
-const email = document.querySelector("#email").value;
-const pwd1 = document.querySelector("#password1").value;
-const pwd2 = document.querySelector("#password2").value;
 
 submitBtn.addEventListener("click", validateForm);
 
 function validateForm(e) {
   e.preventDefault();
+  const name = document.querySelector("#name").value;
+  const email = document.querySelector("#email").value;
+  const pwd1 = document.querySelector("#password1").value;
+  const pwd2 = document.querySelector("#password2").value;
 
-  if (name === "") {
+  if (!name) {
     alert("Name must be filled out");
     return false;
   }
