5f4437a0ff186b67bc6361554de65babd35f6928
Lateef Quadri Olayinka <Lateef9816@gmail.com>
2020-05-28 23:41:28 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,6 +1,20 @@
-const form = document.getElementsByTagName("form");
+const submitBtn = document.querySelector("#submit");
+const name = document.querySelector("#name").value;
+const email = document.querySelector("#email").value;
+const pwd1 = document.querySelector("#password1").value;
+const pwd2 = document.querySelector("#password2").value;
 
-form.addEventListenert("submit", (e) => {
+submitBtn.addEventListener("click", validateForm);
+
+function validateForm(e) {
   e.preventDefault();
-  console.log("aaa");
-});
+
+  if (name === "") {
+    alert("Name must be filled out");
+    return false;
+  }
+  if (email === "") {
+    alert("Email cannot be blank");
+    return false;
+  }
+}
