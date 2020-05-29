5d01cdf92c68d160033235bcd3f3a2e4d4e10fd2
Lateef Quadri Olayinka <Lateef9816@gmail.com>
2020-05-29 01:46:56 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -2,23 +2,38 @@ const submitBtn = document.querySelector("#submit");
 
 submitBtn.addEventListener("click", validateForm);
 
+function displayError(id, message) {
+  document.querySelector(`#${id}`).style.background = "red";
+  document.querySelector(`#${id}`).innerHTML = message;
+  setTimeout(() => {
+    document.querySelector(`#${id}`).innerHTML = "";
+    document.querySelector(`#${id}`).style.background = "none";
+  }, 3000);
+}
 function validateForm(e) {
   e.preventDefault();
   const name = document.querySelector("#name").value;
   const email = document.querySelector("#email").value;
+  const number = document.querySelector("#number").value;
   const pwd1 = document.querySelector("#password1").value;
   const pwd2 = document.querySelector("#password2").value;
 
   if (!name) {
-    alert("Name must be filled out");
-    return false;
+    displayError("name-err", "Name cannot be blank");
   }
-  if (!email) {
-    alert("Email cannot be blank");
-    return false;
+  if (email === "") {
+    displayError("email-err", "Email cannot be blank");
   }
-  if (pwd1.length < 8 || pwd2.length < 8) {
-    alert("Password length cannot be less than 8");
-    return false;
+  if (!email.split("").includes("@")) {
+    displayError("email-err", "Invalid email supplied");
+  }
+  if (!pwd1) {
+    displayError("password-err", "Password cannot be blank");
+  }
+  if (pwd1.length < 8) {
+    displayError("password-err", "Password cannot be less than 8 characters");
+  }
+  if (pwd1 !== pwd2) {
+    displayError("password-err", "Password doesn't match");
   }
 }
